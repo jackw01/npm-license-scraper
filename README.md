d32a09c1721035dd44ad0098c1cf993c805f97d4
jackw01 <jackw01@users.noreply.github.com>
2019-09-09 16:02:55 -0700

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # npm-license-scraper
-Wrapper around npm-license-crawler to download and export text of licenses in JSON format
+
+npm-license-scraper is a wrapper around [npm-license-crawler](https://github.com/mwittig/npm-license-crawler) to download and export text of licenses for your project's dependencies in JSON format.
+
+## Usage
+* `npm i -g npm-license-scraper`
+* Run `npm-license-scraper` in your project folder to generate a JSON file containing the full text of licenses used by your project's dependencies.
---
+++ bin/licensescraper.js
@@ -0,0 +1,34 @@
+// Copyright 2019 jackw01. Released under the MIT License (see LICENSE for details).
+
+const crawler = require('npm-license-crawler');
+const stripAnsi = require('strip-ansi');
+const fs = require('fs');
+const axios = require('axios');
+
+crawler.dumpLicenses({
+  start: ['.'],
+  exclude: [],
+  onlyDirectDependencies: true,
+  omitVersion: true,
+},
+  function (error, licenses) {
+    if (error) console.error('Error:', error);
+    else {
+      const entries = Object.entries(licenses);
+      axios.all(entries.map(([key, value]) => axios.get(value.licenseUrl)))
+      .then((result) => {
+        const out = result.map((value, i) => {
+          const license = value.data
+            .replace(/\ +/g, ' ')
+            .replace(/([^\n]) ?\n ?([^\n])/g, '$1 $2')
+            .trim();
+          return {
+            name: stripAnsi(entries[i][0]),
+            license,
+          };
+        })
+        fs.writeFileSync('./licenses.json', JSON.stringify(out));
+      });
+    }
+  }
+);
